e9632f3d18da1ff43d7d5a4beb3b146007fd4c6d
Owen Cooper <owencoopersfx@gmail.com>
2020-07-01 01:16:48 -0700

    Updates

---
+++ Assets/AudioEventManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioEventManager : MonoBehaviour
+{
+    [System.Serializable]
+    public class AudioEvent
+    {
+        [SerializeField] string name = "New Audio Event";
+        [SerializeField] GameObject[] audioObjects;
+
+    }
+
+    public List<AudioEvent> eventList; 
+}
--- Assets/Scripts/AudioObject.cs
+++ Assets/Scripts/AudioObject.cs
@@ -3,12 +3,65 @@ using System.Collections.Generic;
 using UnityEngine.Audio;
 using UnityEngine;
 
-public class AudioObject : MonoBehaviour, IPooledObject
+public class AudioObject : MonoBehaviour
 {
+    public GameObject voiceObjectPrefab;
+    public string name = "New Audio Object";
+    
+    [Header("Settings")]
+    public int voiceLimit;
+    public enum Positioning{
+        spatialized,
+        flat
+    }
 
+    public Positioning positioning;
+    public bool loop;
+    private AudioSource audioSource;
 
-    public void OnObjectSpawn()
+    void Awake()
     {
-        
+        Init();
+    }
+
+    void Init()
+    {
+        audioSource = GetComponent<AudioSource>();
+
+        var setPositioning = Positioning.spatialized;
+
+        switch (setPositioning)
+        {
+            case Positioning.spatialized:
+                Debug.Log("3D-sound");
+                audioSource.spatialBlend = 1;
+                break;
+            case Positioning.flat:
+                audioSource.spatialBlend = 2;
+                Debug.Log("2D-sound");
+                break;
+            default:
+                break;
+        }
+
+        if (loop)
+        {
+            audioSource.loop = true;
+        }
+    }
+
+    void Start()
+    {
+
+    }
+
+    void PlaySound()
+    {
+
+    }
+
+    void StopSound()
+    {
+
     }
 }
