5b941d0817f8f2d30998a55edd0c4a7e4398889d
Owen Cooper <owencoopersfx@gmail.com>
2020-07-02 01:20:57 -0700

    Continue building AudioObject class. Try accessing it through the AudioEventManager (spoiler alert: it's broken).

--- Assets/AudioEventManager.cs
+++ Assets/AudioEventManager.cs
@@ -7,10 +7,27 @@ public class AudioEventManager : MonoBehaviour
     [System.Serializable]
     public class AudioEvent
     {
-        [SerializeField] string name = "New Audio Event";
-        [SerializeField] GameObject[] audioObjects;
-
+        public string Name = "New Audio Event";
+        public AudioObject[] AudioObjects;
     }
 
-    public List<AudioEvent> eventList; 
+    public List<AudioEvent> eventList;
+
+    private AudioEvent audioEvent;
+
+    void Awake()
+    {
+        audioEvent = new AudioEvent();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            for (int i = 0; i < (audioEvent.AudioObjects.Length - 1); i++)
+            {
+                audioEvent.AudioObjects[i].PlaySound();
+            }
+        }
+    }
 }
--- Assets/Scripts/AudioObject.cs
+++ Assets/Scripts/AudioObject.cs
@@ -5,19 +5,29 @@ using UnityEngine;
 
 public class AudioObject : MonoBehaviour
 {
-    public GameObject voiceObjectPrefab;
-    public string name = "New Audio Object";
-    
-    [Header("Settings")]
-    public int voiceLimit;
-    public enum Positioning{
-        spatialized,
-        flat
-    }
+    public string Name = "New Audio Object";
 
-    public Positioning positioning;
+    #region Notes
+    //FUTURE ITERATION: Make new editor that adapts to the ObjectType enum.
+    //public enum ObjectType { simple, randomContainer, sequenceContainer }
+    //public ObjectType objectType;
+    #endregion
+
+    [Header("Settings")]
+    [SerializeField] private int voiceLimit = 3;
+    private enum Positioning { _3D, _2D }
+    [SerializeField] private Positioning positioning;
     public bool loop;
-    private AudioSource audioSource;
+    //public AudioMixerGroup output;
+
+    [Header("Assets")]
+    public AudioClip[] audioClips;
+
+    private List<AudioSource> voices;
+
+    //private AudioSource defaultAudioSource;
+
+    private AudioSource _voice;
 
     void Awake()
     {
@@ -26,44 +36,82 @@ public class AudioObject : MonoBehaviour
 
     void Init()
     {
-        audioSource = GetComponent<AudioSource>();
-
-        var setPositioning = Positioning.spatialized;
-
-        switch (setPositioning)
-        {
-            case Positioning.spatialized:
-                Debug.Log("3D-sound");
-                audioSource.spatialBlend = 1;
-                break;
-            case Positioning.flat:
-                audioSource.spatialBlend = 2;
-                Debug.Log("2D-sound");
-                break;
-            default:
-                break;
-        }
-
-        if (loop)
-        {
-            audioSource.loop = true;
-        }
+        CreateVoices(voiceLimit);
+        SetPositioning(positioning);
     }
 
     void Start()
     {
-        //adding a comment to test commit after moving .gitignore
-        //test again. If Librarby and assembly files keep showing up, consider adding to .gitignore or stop tracking.
-        //test again after using stop tracking.
+
     }
 
-    void PlaySound()
+    public void PlaySound()
     {
+        //TODO: pool voices by:
+        //  1. enable AudioSource, Enqueue in Dictionary
+        //  2. Load clip and play
+        //  3. disable AudioSource after clip is finished, Dequeue in Dictionary
+        //OR: Try todo this just using the list.
 
+        //TODO: Try just dynamically adding and removing one AudioSource for each voice to be played. Probably less performant, but worth testing.
+
+        int voiceToPlay = 0;
+        int randomClip = Random.Range(0, audioClips.Length);
+
+        AudioSource audioSource = voices[voiceToPlay];
+
+        audioSource.clip = audioClips[randomClip];
+        audioSource.Play();
     }
 
     void StopSound()
     {
 
+    }
+
+    void CreateVoices(int _voiceLimit)
+    {
+        voices = new List<AudioSource>();
+
+        if (_voiceLimit <= 0)
+        {
+            _voiceLimit = 1;
+            Debug.LogWarning("Invalid voiceLimit! Set to default: 1.");
+        }
+
+        if (_voiceLimit > 0)
+        {
+            for (int i = 0; i < _voiceLimit; i++)
+            {
+                _voice = gameObject.AddComponent<AudioSource>();
+                _voice.enabled = false;
+
+                if (loop)
+                {
+                    _voice.loop = true;
+                }
+
+                voices.Add(_voice);
+            }
+        }
+    }
+
+    void SetPositioning(Positioning _positioning)
+    {
+        Positioning setPositioning = _positioning;
+
+        switch (setPositioning)
+        {
+            case Positioning._3D:
+                Debug.Log("3D-sound [" + Name + "] initialized. " + "Voice Limit: " + voiceLimit);
+                _voice.spatialBlend = 1;
+                break;
+            case Positioning._2D:
+                _voice.spatialBlend = 2;
+                Debug.Log("2D-sound [" + Name + "] initialized. " + "Voice Limit: " + voiceLimit);
+                break;
+            default:
+                break;
+        }
     }
 }
